0b9780315a429a9d388e03f730a544f44d6907f1
imrebalai <imrebalai@gmail.com>
2020-07-14 08:28:30 +0200

    Part3

--- main.go
+++ main.go
@@ -3,22 +3,19 @@ package main
 import (
 	"context"
 	"log"
+	"microservices/handlers"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
-
-	"./handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
 
+	ph := handlers.NewProducts(l)
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
+	sm.Handle("/", ph)
 
 	s := &http.Server{
 		Addr:         ":9090",
